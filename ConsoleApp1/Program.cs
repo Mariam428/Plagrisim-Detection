8f80d97c2d76ecd57c7401156005e86b150d8c9f
mariam <mariamsalah478@gmail.com>
2024-05-06 00:50:42 +0300

    stat

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -52,7 +52,7 @@ namespace ConsoleApp1
                 }
             }
         }
-
+        
         static Dictionary<KeyValuePair<string, string>, string> ConstructTheGraph(Dictionary<KeyValuePair<string, string>, List<string>> dictionary)
         {
             Dictionary<KeyValuePair<string, string>, string> Graph = new Dictionary<KeyValuePair<string, string>, string>();
@@ -73,22 +73,91 @@ namespace ConsoleApp1
                 Graph.Add(key1, value1);
                 Graph.Add(key2, value2);
             }
+
             return Graph;
+        }
+        static HashSet<HashSet<string>> GetComponents(Dictionary<KeyValuePair<string, string>, string> graph)
+        {
+
+            HashSet<HashSet<string>> components = new HashSet<HashSet<string>>();
+            HashSet<string> visited = new HashSet<string>();
+
+            foreach (var node in graph.Keys)
+            {
+                if (!visited.Contains(node.Key))
+                {
+                    HashSet<string> component = new HashSet<string>();
+                    DepthFirstSearch(graph, node.Key, visited, component);
+                    components.Add(component);
+                }
+            }
+            return components;
+        }
+        static void DepthFirstSearch(Dictionary<KeyValuePair<string, string>, string> graph, string currentNode, HashSet<string> visited, HashSet<string> component)
+        {
+            visited.Add(currentNode);
+            component.Add(currentNode);
+            var adjacentNodes = new List<string>();
+            foreach (var edge in graph)
+            {
+                if ((edge.Key.Key == currentNode || edge.Key.Value == currentNode) && !visited.Contains(edge.Key.Key))
+                {
+                    if (edge.Key.Key == currentNode)
+                    {
+                        adjacentNodes.Add(edge.Key.Value);
+                    }
+                    else
+                    {
+                        adjacentNodes.Add(edge.Key.Key);
+                    }
+                }
+            }
+            foreach (var node in adjacentNodes)
+            {
+                DepthFirstSearch(graph, node, visited, component);
+            }
+        }
+
+        static void calculateStat(Dictionary<KeyValuePair<string, string>, string> graph)
+        {
+            HashSet<HashSet<string>> components = GetComponents(graph);
+
+            double total=0;
+            double avg = 0;
+            double count = 0;
+            foreach (var component in components)
+            {
+                total = 0;
+                count = 0;
+              //  Console.WriteLine("Component:");
+                foreach (var node in component)
+                {
+                    Console.WriteLine(node);
+                    // Loop through edges in the graph
+                    foreach (var edge in graph)
+                    {
+                        // Check if the edge connects to the current node and if the other end of the edge is also in the component
+                        if (edge.Key.Key == node)
+                        {
+                            // Access the weight of the edge
+                            string weight = edge.Value;
+                            weight = weight.Replace("%", "");
+                            double w = double.Parse(weight); 
+                            total += w;
+                            count++;
+                        }
+                    }
+                }
+                    avg = total / count;
+            }
         }
 
         static void Main(string[] args)
         {
-            readFile(@"D:\3rd\Algorithm\Plagrisim-Detection\ConsoleApp1\Sample\1-Input.xlsx");
+            readFile(@"D:\3rd\Algorithm\Plagrisim-Detection\ConsoleApp1\Sample\6-Input.xlsx");
             Dictionary<KeyValuePair<string, string>, string> graph;
             graph=ConstructTheGraph(myDictionary);
-            Console.WriteLine(graph.Keys.Count);
-            foreach (var entry in graph)
-            {
-                Console.WriteLine($"Hyperlink 1: {entry.Key.Key}");
-                Console.WriteLine($"Hyperlink 2: {entry.Key.Value}");
-                Console.WriteLine($"Similarity : {entry.Value}");
-                Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-            }
+            calculateStat(graph);
         }
     }
 }
